eef3504df7277761efcafef82b46b3b06ba9787e
bruna e julia <brujure2023@gmail.com>
2024-06-07 14:36:51 -0300

    Arredondar as bordas da tela branca e colocar a  imagem no botão. Começar a programação

---
+++ app/src/main/java/artur/renata/dreamlock/NomesSalaActivity.java
@@ -0,0 +1,14 @@
+package artur.renata.dreamlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NomesSalaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nomes_sala);
+    }
+}
