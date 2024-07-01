bd2913ead22090a38070c1630979dc028c23ed47
artcarvalho <arturcarvalho05@gmail.com>
2024-07-01 01:39:02 -0300

    fix read NFC ID (reversed read)

--- app/src/main/java/artur/renata/dreamlock/activity/CadastroActivity.java
+++ app/src/main/java/artur/renata/dreamlock/activity/CadastroActivity.java
@@ -76,18 +76,23 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
 
-    private String getHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = bytes.length - 1; i >= 0; --i) {
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-            if (i > 0) {
-                sb.append(" ");
-            }
-        }
-        return sb.toString().toUpperCase();
+    private String getHex(byte [] inarray) {
+        int i, j, in;
+        String [] hex = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        String out= "";
 
+        for(j = 0 ; j < inarray.length ; ++j)
+        {
+            in = (int) inarray[j] & 0xff;
+            i = (in >> 4) & 0x0f;
+            out += hex[i];
+            i = in & 0x0f;
+            out += hex[i];
+            out+=" ";
+        }
+        return out;
     }
+
+
 }
+
